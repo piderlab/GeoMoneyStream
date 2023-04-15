b08d8ba5accba6b13b02db571a2d831b9ab1517c
ayame113 <40050810+ayame113@users.noreply.github.com>
2023-04-15 16:15:42 +0900

    update flowRate logic (#2)
    * update flowRate logic
    
    * fix flowRate logic
    
    * update text color
    
    * format variables
    
    * fix margin

--- islands/Map.tsx
+++ islands/Map.tsx
@@ -24,7 +24,6 @@ if (IS_BROWSER) {
   });
   carIcon = L.icon({
     iconUrl: `/car_icon.png`,
-    // iconRetinaUrl: `${leafletUrl}dist/images/marker-icon-2x.png`,
     shadowUrl: `${leafletUrl}dist/images/marker-shadow.png`,
     iconSize: [60, 60],
     iconAnchor: [30, 30],
@@ -34,34 +33,68 @@ if (IS_BROWSER) {
   });
 
   // init stylesheet
-  const styleUrl = `${leafletUrl}dist/leaflet.css`;
   const leafletStyleElement = document.createElement("link");
   leafletStyleElement.type = "text/css";
   leafletStyleElement.rel = "stylesheet";
-  leafletStyleElement.href = styleUrl;
+  leafletStyleElement.href = `${leafletUrl}dist/leaflet.css`;
   document.head.append(leafletStyleElement);
-  // styleElement.sheet = leafletStyle;
-  // const leafletStyle = new CSSStyleSheet();
-  // (async () => { // avoid tla
-  //   const res = await fetch(styleUrl);
-  //   leafletStyleElement.sheet!.replace(await res.text());
-  // })();
 }
 
-// const defaultLat = 35.654246;
-// const defaultLon = 139.741146;
-const 地図中心: [number, number] = [35.664883, 139.725265];
+// マーカー設置地点一覧
+const points = {
+  地図中心: [35.664883, 139.725265],
+  虎ノ門ヒルズ: [35.6671145, 139.7499026],
+  渋谷: [35.6579364, 139.7017251],
+  信濃町: [35.68, 139.72], //hh
+  泉岳寺: [35.64, 139.74], //hh
+} satisfies Record<string, [number, number]>;
 
-const 虎ノ門ヒルズ: [number, number] = [35.6671145, 139.7499026];
+// 車のスタート/ゴール地点
+const スタート地点 = points.渋谷;
+const ゴール地点 = points.虎ノ門ヒルズ;
 
-// const 六本木ヒルズLat = 35.6594175;
-// const 六本木ヒルズLon = 139.7301143;
-const 渋谷: [number, number] = [35.6579364, 139.7017251];
+// 支払いエリア設定
+/** デフォルト（ベース）の支払額 */
+const defaultFlowRate = { in: 0, out: 40 };
 
-const pointA: [number, number] = [35.68, 139.72]; //hh
-const pointB: [number, number] = [35.64, 139.74]; //hh
+const areas: Area[] = [
+  {
+    // 中心座標
+    center: points.虎ノ門ヒルズ,
+    flowRateList: [{
+      // 半径2000m以内の時は、defaultFlowRateの値に加えて10トークンのincoming
+      radius: 2000,
+      flowRate: { in: 10, out: 0 },
+      color: "blue",
+      fillColor: "#93C5FD",
+    }, {
+      // 半径1000m以内の時は、defaultFlowRateの値に加えて20トークンのincoming
+      radius: 1000,
+      flowRate: { in: 20, out: 0 },
+      color: "green",
+      fillColor: "#A7F3D0",
+    }, {
+      // 半径500m以内の時は、defaultFlowRateの値に加えて30トークンのincoming
+      radius: 500,
+      flowRate: { in: 30, out: 0 },
+      color: "red",
+      fillColor: "#FCA5A5",
+    }],
+  },
+];
+
+interface Area {
+  center: [number, number];
+  flowRateList: {
+    radius: number;
+    flowRate: { in: number; out: number };
+    color: string;
+    fillColor: string;
+  }[];
+}
 
 interface MapProps {
+  flowRate: Signal<{ in: number; out: number }>;
   distanceToParking: Signal<number>;
   walletAddress: Signal<string | null>;
 }
@@ -69,7 +102,7 @@ interface MapProps {
 export default function Map(props: MapProps) {
   const divRef = useRef<HTMLDivElement>(null);
   useEffect(() => {
-    const map = L.map(divRef.current!).setView(地図中心, 13);
+    const map = L.map(divRef.current!).setView(points.地図中心, 13);
 
     L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
       attribution:
@@ -78,67 +111,71 @@ export default function Map(props: MapProps) {
     L.control.scale().addTo(map);
 
     // アイコン追加
-    L.marker(虎ノ門ヒルズ, { icon: blueIcon }).addTo(map);
-    L.marker(渋谷, { icon: blueIcon }).addTo(map);
-    L.marker(pointA, { icon: blueIcon }).addTo(map); //hh
-    L.marker(pointB, { icon: blueIcon }).addTo(map); //hh
+    for (const point of Object.values(points)) {
+      L.marker(point, { icon: blueIcon }).addTo(map);
+    }
 
-    // 円
-    L.circle(虎ノ門ヒルズ, {
-      radius: 2000,
-      color: "blue",
-      fillColor: "#93C5FD",
-      fillOpacity: 0.5,
-      weight: 0,
-    }).addTo(map);
-    L.circle(虎ノ門ヒルズ, {
-      radius: 1000,
-      color: "green",
-      fillColor: "#A7F3D0",
-      fillOpacity: 0.5,
-      weight: 0,
-    }).addTo(map);
-    L.circle(虎ノ門ヒルズ, {
-      radius: 500,
-      color: "red",
-      fillColor: "#FCA5A5",
-      fillOpacity: 0.5,
-      weight: 0,
-    }).addTo(map);
-    //L.polyline([虎ノ門ヒルズ, 渋谷], {
-    //  color: "#000000",
-    //  weight: 3,
-    //}).addTo(map);
+    // 円を追加
+    for (const area of areas) {
+      for (const { radius, color, fillColor } of area.flowRateList) {
+        L.circle(area.center, {
+          radius,
+          color,
+          fillColor,
+          fillOpacity: 0.5,
+          weight: 0,
+        }).addTo(map);
+      }
+    }
 
     // 車のアイコン
-    const 渋谷Marker = L.Marker.movingMarker([渋谷, 虎ノ門ヒルズ], [20000], {
-      draggable: true,
-      icon: carIcon!,
-    })
-      .addTo(map);
-    渋谷Marker.on("mousedown", () => {
-      渋谷Marker.stop();
-    });
-    渋谷Marker.on("mouseup", () => {
-      渋谷Marker.moveTo(虎ノ門ヒルズ, 20000);
-    });
+    const carMarker = L.Marker
+      .movingMarker([スタート地点, ゴール地点], [20000], {
+        draggable: true,
+        icon: carIcon,
+      }).addTo(map)
+      .on("mousedown", () => carMarker.stop())
+      .on("mouseup", () => carMarker.moveTo(ゴール地点, 20000))
+      .on("move", (e: unknown) => {
+        const { latlng } = e as L.LeafletMouseEvent;
+        const distanceToParking = latlng.distanceTo(ゴール地点);
+        props.distanceToParking.value = distanceToParking;
+        // ゴールまで1mを切っている場合は支払い終了（0.xxxMの時の小数点以下がわかりにくいため）
+        props.flowRate.value = 1 < distanceToParking
+          ? flowRateFromLatLng(latlng)
+          : { in: 0, out: 0 };
+      });
+
     props.walletAddress.subscribe((newValue) => {
       if (newValue) {
-        渋谷Marker.start();
+        carMarker.start();
       }
     });
-
-    渋谷Marker.on("move", (e: unknown) => {
-      const distance = (e as L.LeafletMouseEvent).latlng.distanceTo(
-        虎ノ門ヒルズ,
-      );
-      // props.setDistanceToParking(distance);
-      props.distanceToParking.value = distance;
-    });
   }, []);
-  return (
-    <>
-      <div class="w-1/2 h-screen" ref={divRef}></div>
-    </>
-  );
+  return <div class="w-1/2 h-screen" ref={divRef}></div>;
+}
+
+function flowRateFromLatLng(latlng: L.LatLng) {
+  const res = areas.map((area) => {
+    const distance = latlng.distanceTo(area.center);
+    let areaFlowRate = { in: 0, out: 0 };
+    let minRadius = Infinity;
+    for (const { radius, flowRate } of area.flowRateList) {
+      if (distance < radius && radius < minRadius) {
+        areaFlowRate = flowRate;
+        minRadius = radius;
+      }
+    }
+    return areaFlowRate;
+  }).reduce((prev, current) => ({
+    in: prev.in + current.in,
+    out: prev.out + current.out,
+  }), defaultFlowRate);
+
+  if (res.out < res.in) {
+    // 収入の方が大きい場合はNG(バグ)
+    // outにclampする
+    return { in: res.out, out: res.out };
+  }
+  return res;
 }
